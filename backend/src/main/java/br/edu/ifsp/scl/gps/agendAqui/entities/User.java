e59a04cfd4542db07ca10d2f45033b0bbe4b6fb2
Lygia Bueno <lybueno@gmail.com>
2022-04-07 15:29:31 -0300

    Role, User created. H2 properties

---
+++ backend/src/main/java/br/edu/ifsp/scl/gps/agendAqui/entities/User.java
@@ -0,0 +1,119 @@
+package br.edu.ifsp.scl.gps.agendAqui.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String NomeCompleto;
+	private String email;
+	private String cpf;
+	private String telefone;
+	private String senha;
+	
+	@ManyToMany
+	@JoinTable(name = "tb_user_roles",
+				joinColumns = @JoinColumn(name = "user_id"),
+				inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
+	
+	public User() {
+	}
+
+	public User(Long id, String nomeCompleto, String email, String cpf, String telefone, String senha) {
+		this.id = id;
+		NomeCompleto = nomeCompleto;
+		this.email = email;
+		this.cpf = cpf;
+		this.telefone = telefone;
+		this.senha = senha;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeCompleto() {
+		return NomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		NomeCompleto = nomeCompleto;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+	
+}
