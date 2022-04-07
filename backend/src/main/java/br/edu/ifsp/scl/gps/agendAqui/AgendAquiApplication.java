ef136173f51672968757f9b10324e0a1423d033c
Lygia Bueno <lybueno@gmail.com>
2022-04-07 14:39:24 -0300

    Backend project created

---
+++ backend/src/main/java/br/edu/ifsp/scl/gps/agendAqui/AgendAquiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.scl.gps.agendAqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendAquiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendAquiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/edu/ifsp/scl/gps/agendAqui/AgendAquiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.scl.gps.agendAqui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendAquiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
