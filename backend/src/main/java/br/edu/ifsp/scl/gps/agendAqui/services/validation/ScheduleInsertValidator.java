bec84f3018238572788c29856fce8152a695a2ae
renan.oliveira <renan.oliveira.rao@gmail.com>
2022-04-27 20:23:15 -0300

    Implementing Schedule validators

--- backend/src/main/java/br/edu/ifsp/scl/gps/agendAqui/services/validation/ScheduleInsertValidator.java
+++ backend/src/main/java/br/edu/ifsp/scl/gps/agendAqui/services/validation/ScheduleInsertValidator.java
@@ -1,14 +1,18 @@
 package br.edu.ifsp.scl.gps.agendAqui.services.validation;
 
 import br.edu.ifsp.scl.gps.agendAqui.dto.ScheduleInsertDTO;
+import br.edu.ifsp.scl.gps.agendAqui.entities.Schedule;
+import br.edu.ifsp.scl.gps.agendAqui.entities.User;
 import br.edu.ifsp.scl.gps.agendAqui.repositories.ScheduleRepository;
+import br.edu.ifsp.scl.gps.agendAqui.resources.exceptions.FieldMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ScheduleInsertValidator implements ConstraintValidator<ScheduleInsertValid, ScheduleInsertDTO> {
-    //TODO: Implementar classe que eu não entendi nadaaaa
 
     @Autowired
     private ScheduleRepository repository;
@@ -19,7 +23,30 @@ public class ScheduleInsertValidator implements ConstraintValidator<ScheduleInse
     }
 
     @Override
-    public boolean isValid(ScheduleInsertDTO scheduleInsertDTO, ConstraintValidatorContext constraintValidatorContext) {
-        return false;
+    public boolean isValid(ScheduleInsertDTO scheduleInsertDTO, ConstraintValidatorContext context) {
+        List<FieldMessage> list = new ArrayList<>();
+
+        List<Schedule> schedules = repository.findByProvider(scheduleInsertDTO.getProvider());
+
+
+        // Coloque aqui seus testes de validação, acrescentando objetos FieldMessage à lista
+
+        if(schedules.isEmpty()) {
+            list.add(new FieldMessage("agenda", "Nenhum horário disponível"));
+        }
+
+        schedules.forEach(
+                schedule -> {
+                    if(schedule.getDate().equals(scheduleInsertDTO.getDate())){
+                        list.add(new FieldMessage("agenda", "Horário não disponível"));
+                    }
+                });
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+                    .addConstraintViolation();
+        }
+        return list.isEmpty();
     }
 }
--- backend/src/main/java/br/edu/ifsp/scl/gps/agendAqui/services/validation/ScheduleUpdateValidator.java
+++ backend/src/main/java/br/edu/ifsp/scl/gps/agendAqui/services/validation/ScheduleUpdateValidator.java
@@ -1,12 +1,28 @@
 package br.edu.ifsp.scl.gps.agendAqui.services.validation;
 
 import br.edu.ifsp.scl.gps.agendAqui.dto.ScheduleUpdateDTO;
+import br.edu.ifsp.scl.gps.agendAqui.entities.Schedule;
+import br.edu.ifsp.scl.gps.agendAqui.entities.User;
+import br.edu.ifsp.scl.gps.agendAqui.repositories.ScheduleRepository;
+import br.edu.ifsp.scl.gps.agendAqui.resources.exceptions.FieldMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public class ScheduleUpdateValidator implements ConstraintValidator<ScheduleUpdateValid, ScheduleUpdateDTO> {
-    //TODO: ver como implementa pq eu nao entendi tbm
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private ScheduleRepository repository;
 
     @Override
     public void initialize(ScheduleUpdateValid constraintAnnotation) {
@@ -14,7 +30,31 @@ public class ScheduleUpdateValidator implements ConstraintValidator<ScheduleUpda
     }
 
     @Override
-    public boolean isValid(ScheduleUpdateDTO scheduleUpdateDTO, ConstraintValidatorContext constraintValidatorContext) {
-        return false;
+    public boolean isValid(ScheduleUpdateDTO scheduleUpdateDTO, ConstraintValidatorContext context) {
+
+        @SuppressWarnings("unchecked")
+        var uriVars = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        long scheduleId = Long.parseLong(uriVars.get("id"));
+
+        List<FieldMessage> list = new ArrayList<>();
+
+        Optional<Schedule> schedule = repository.findByDate(scheduleUpdateDTO.getDate());
+
+        // Coloque aqui seus testes de validação, acrescentando objetos FieldMessage à lista
+
+        if(schedule.isEmpty()) {
+            list.add(new FieldMessage("agendamento", "Sem agendamentos para essa data"));
+        }else{
+            if( schedule.get().getClient() != null){
+                list.add(new FieldMessage("agendamento", "Data e horario ocupado"));
+            }
+        }
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+                    .addConstraintViolation();
+        }
+        return list.isEmpty();
     }
 }
